cd11c2f44fd4526e3f9a61fc628ab7387c8294b6
RehaBugrahanOzel <bugrahanozel1996@gmail.com>
2023-04-03 20:38:12 +0300

    initial page design completed

---
+++ posture-fix/src/page/InitialPage.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="initial">
+    <div class="title-section">
+      <img src="../assets/logo.svg" alt="logo" class="logo" />
+      <div class="title">PostureFix</div>
+    </div>
+    <div class="first-cart">
+      <CommonButton text="Login" class="button" wrapper="dark" />
+    </div>
+    <div class="second-cart">
+      <CommonButton text="Register" class="button" wrapper="light" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonButton from "../components/CommonButton.vue";
+import "../assets/css/style.css";
+export default {
+  name: "InitialPage",
+  components: {
+    CommonButton,
+  },
+};
+</script>
+
+<style>
+.initial {
+  background: #cfe2fa;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+}
+.title-section {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  margin-top: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  width: 157px;
+  height: 182px;
+  margin: 16px;
+}
+.title {
+  font-weight: bolder;
+  font-size: 48px;
+  line-height: 58px;
+  line-height: 100%;
+  font-family: "Arial Black";
+}
+.first-cart {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  transform: translate(-50%, 0);
+  height: 46%;
+  max-height: 406px;
+  width: 100%;
+  bottom: 0;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  border-radius: 32px 32px 0 0;
+}
+.first-cart .button {
+  width: 80%;
+  max-width: 330px;
+  height: 56px;
+  margin-top: 7%;
+}
+.second-cart {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  height: 30%;
+  max-height: 287px;
+  width: 100%;
+  bottom: 0;
+  background-color: #000000;
+  border-radius: 32px 32px 0 0;
+  display: flex;
+  justify-content: center;
+}
+.second-cart .button {
+  width: 80%;
+  max-width: 330px;
+  height: 56px;
+  margin: 7%;
+}
+</style>
